a41559559535d14f4f154cd5d4c75ecdc3a7b8e2
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-13 14:25:49 +0530

    sum update

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,28 +1,13 @@
 # include <iostream>
 using namespace std;
 int main(){
-    // int n=5;
-    // int count =1;
-    // while (count <=n)
-    // {
-    //     cout<<"* "<<endl;
-    //     //count++;
-    // }
-
-    // for loop 
-    // int n=5;
-    // for(int i=0;i<=n;i++){
-    //     cout<<i<<endl;
-    // }
-    //sum n numners
-    int sum=0;
-    int n;
-    cout<<"enetr n:";
-    cin>>n;
-    for(int i=1;i<=n;i++){
+  int n=10;
+  int sum=0;
+  for(int i=1; i<=n;i++){
+    if(i%2 ==0){
         sum+=i;
-        //cout<<sum<<" ";
-    }
-    cout<<sum<<endl;
+    } 
+  }
+  cout<<sum<<endl;
     return 0;
 }
