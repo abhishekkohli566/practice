2b6951762fd26a4ebe634bce5c8abf05db05b81f
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-13 11:28:02 +0530

    upadate

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,12 +1,28 @@
 # include <iostream>
 using namespace std;
 int main(){
-    int n=5;
-    int count =1;
-    while (count <=n)
-    {
-        cout<<"* "<<endl;
-        count++;
+    // int n=5;
+    // int count =1;
+    // while (count <=n)
+    // {
+    //     cout<<"* "<<endl;
+    //     //count++;
+    // }
+
+    // for loop 
+    // int n=5;
+    // for(int i=0;i<=n;i++){
+    //     cout<<i<<endl;
+    // }
+    //sum n numners
+    int sum=0;
+    int n;
+    cout<<"enetr n:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        sum+=i;
+        //cout<<sum<<" ";
     }
+    cout<<sum<<endl;
     return 0;
 }
