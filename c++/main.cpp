d785a3a58fe40b5f7676b8b42a11f73418b03380
Abhishek Kohli <abhishek.25b01010165@abes.ac.in>
2026-09-14 22:04:52 +0530

    update

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,21 +1,62 @@
-# include <iostream>
+// # include <iostream>
+// using namespace std;
+// int main(){
+//   int n=10;
+//   bool isprime=true;
+//   for(int i=2; i<=n-1;i++){
+//     if(n%i==0){
+//       isprime=false;
+//       break;
+  
+//     } 
+//   }
+//    if(isprime==true){
+//     cout<<"prime no";
+//    }
+//    else{
+//     cout<<"non prime";
+//    }
+  
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int n;
+// for(int i=0;i<=10;i++){
+//  // int m=1;
+//   for(int i=1;i<=20;i++){
+//     cout<<" hlo";
+//   }
+//   cout<<endl;
+// }
+// cout<<endl;
+
+// }/
+//factorial
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int n;
+//   cin>>n;
+//   long  factorial=1;
+
+//   for(int i=1;i<=n;i++){
+//     factorial*=i;
+      
+      
+//   }
+//   cout<<factorial<<endl;
+//   return 0;
+// }
+
+//square pattern
+#include<iostream>
 using namespace std;
 int main(){
-  int n=10;
-  bool isprime=true;
-  for(int i=2; i<=n-1;i++){
-    if(n%i==0){
-      isprime=false;
-      break;
-  
-    } 
-  }
-   if(isprime==true){
-    cout<<"prime no";
-   }
-   else{
-    cout<<"non prime";
-   }
-  
-    return 0;
+    int n;
+
 }
